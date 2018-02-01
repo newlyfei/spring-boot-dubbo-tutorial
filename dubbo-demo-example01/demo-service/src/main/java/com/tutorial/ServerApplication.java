62132e409fcba78fb3eb904c5eb6a3b15e6a2235
newlyfei <newlyfei@163.com>
2018-02-01 18:40:20 +0800

    init update

--- dubbo-demo-example01/demo-service/src/main/java/com/tutorial/ServerApplication.java
+++ dubbo-demo-example01/demo-service/src/main/java/com/tutorial/ServerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-@DubboComponentScan(basePackages = "com.tutorial.dubbo.impl")
 public class ServerApplication
 {
     public static void main( String[] args )
